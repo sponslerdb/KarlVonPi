526a53610d8dc8f7601618f3a1506fa3ce66be43
Doug Sponsler <sponslerdb@gmail.com>
2019-01-29 17:17:51 -0500

    set up file time stamping, b&w video, lower res

--- video.py
+++ video.py
@@ -1,1 +1,15 @@
+from picamera import PiCamera
 import time
+import datetime
+
+camera = PiCamera()
+
+camera.framerate = 30
+camera.resolution = (640, 480)
+camera.color_effects = (128, 128)
+camera.start_preview()
+tstamp = datetime.datetime.now().strftime("%Y-%m-%d-%H:%M:%S")
+camera.start_recording('KarlPi_' + tstamp + '.h264')
+time.sleep(20)
+camera.stop_recording()
+camera.stop_preview()
