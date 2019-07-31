0448d478b8ae950b15abb0a62e423cdb1c6d06c1
Doug Sponsler <33326693+sponslerdb@users.noreply.github.com>
2019-07-31 10:35:30 -0400

    increased res, reduced freq

--- video.py
+++ video.py
@@ -17,16 +17,15 @@ log.addHandler(h)
 # Camera settings; run these outside the job loop
 camera = PiCamera()
 camera.framerate = 30 # 30 fps
-camera.resolution = (640, 480) # lower than default to minimize file size
+#camera.resolution = (640, 480) # lower than default to minimize file size
 camera.color_effects = (128, 128) # b&w
 
 # Prep sched command
 sched = BlockingScheduler()
 
-# run script every 15 minutes between 10am and 2pm, 7 days/wk
+# run script every 60 minutes between 10am and 4pm, 7 days/wk
 @sched.scheduled_job('cron', day_of_week='mon-sun', 
-                     hour='10-14', minute='0-59/15', 
-                     timezone='America/New_York')
+                     hour='10-16', timezone='America/New_York')
 def scheduled_job():
     camera.start_preview()
     tstamp = datetime.datetime.now().strftime("%Y-%m-%d-%H:%M:%S") # get timestamp
@@ -34,8 +33,6 @@ def scheduled_job():
     time.sleep(120) # record 2 minute video
     camera.stop_recording()
     camera.stop_preview()
-
-# Need to figure out how to subprocess the output to my server
 
 sched.start()
 
